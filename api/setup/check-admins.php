1159098a9d2a3deaec55951a6ffe5598c6ec5cfc
WellCore Fitness <info@wellcorefitness.com>
2026-03-09 08:36:18 -0500

    chore: eliminar script temporal check-admins

--- api/setup/check-admins.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/../config/database.php';
-header('Content-Type: text/plain');
-$db = getDB();
-$admins = $db->query("SELECT id, username, name, role FROM admins ORDER BY id")->fetchAll(PDO::FETCH_ASSOC);
-foreach ($admins as $a) {
-    echo "{$a['id']} | {$a['username']} | {$a['name']} | {$a['role']}\n";
-}
